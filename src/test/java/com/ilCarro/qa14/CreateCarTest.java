2fd7acc51df57c4da5ef2b69ef8aefc408f57380
Svetlana-Lappo <pushinkas@gmail.com>
2021-06-08 21:05:39 +0200

    added new Clases Car, CreateCarTest and User

---
+++ src/test/java/com/ilCarro/qa14/CreateCarTest.java
@@ -0,0 +1,30 @@
+package com.ilCarro.qa14;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class CreateCarTest extends TestBase{
+
+    @BeforeMethod
+    public void ensurePrecondition(){
+        if(!isUserLoggedIn()){
+            logIn();
+        }
+    }
+
+    @Test
+    public void addCarTest() throws InterruptedException{
+        pause();
+        clickOnAddCarTab();
+
+        fillCarForm(new Car()
+                .setCountry("Germany")
+                .setAddress("Friedrichstrasse")
+                .setDistance("1000"));
+
+
+        clickOnButtonSubmitOnCarAddPage();
+    }
+
+}
